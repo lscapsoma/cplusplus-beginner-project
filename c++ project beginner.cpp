115fabfa4f820e5efcd8a76d4d3678d586772d94
lscapsoma <123836589+lscapsoma@users.noreply.github.com>
2023-02-02 20:05:21 -0700

    Add files via upload

---
+++ c++ project beginner.cpp
@@ -0,0 +1,27 @@
+
+
+
+#include "std_lib_facilities.h"
+
+
+int main()
+{
+
+	int val1;
+	int val2;
+	cout << "please enter the first integer: ";
+	cin >> val1;
+	cout << "Please enter the second integer: ";
+	cin >> val2;
+	cout << val1 + val2 << " is the sum of " << val1 << " and " << val2 << "\n";
+	cout << val1 - val2 << " is the difference(subtraction) of " << val1 << " and " << val2 << "\n";
+	cout << val1 * val2 << " is the product of " << val1 << " and " << val2 << "\n";
+	co
+
+
+		if (val1 < val2)
+			cout << val1 << " is smaller than " << val2 << "\n";
+	if (val1 > val2)
+		cout << val1 << " is greater than " << val2;
+
+}
